c3642641483de1c1f3058ad92db1099a63803317
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-19 17:37:52 +0100

    feat: add basic js logic and html content

--- js/script.js
+++ js/script.js
@@ -1,6 +1,44 @@
 /*
 Descrizione:
-Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Visualizzare in pagina 5 numeri casuali. 
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, 
+uno alla volta, i numeri che ha visto precedentemente, 
+tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e 
+quali dei numeri da indovinare sono stati individuati.
 */
+
+
+let numbersContainer = document.querySelector('.numbersContainer');
+
+let countdown = document.querySelector('.countdown');
+
+let startGame = document.querySelector('.startGame');
+
+let numbersArray = [];
+
+timer = 30;
+
+
+startGame.addEventListener('click', function() {
+
+    for (let index = 0; index < 5; index++) {
+        let randomNum = Math.floor(Math.random() * 100);
+        numbersArray.push(randomNum);    
+    }
+    numbersContainer.innerHTML = numbersArray;
+    
+    const clock = setInterval(function() {
+        timer--;
+        countdown.innerHTML = timer;
+        if (timer == 0) {
+            clearInterval(clock);
+            numbersContainer.innerHTML = '';
+        }
+    }, 1000);
+    
+});
+
+
+
